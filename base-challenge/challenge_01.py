a86bcc64363ed2879047713f553dd2f28fe88ae5
dlottermann <diegolottermann@gmail.com>
2024-12-02 19:45:40 -0300

    add chanllenge and example openai

---
+++ base-challenge/challenge_01.py
@@ -0,0 +1,8 @@
+name = input("Digite seu nome: ")
+age = input("Digite sua idade: ")
+
+print(f"Olá, {name}!")
+print(f"Seu nome tem {len(name)} letras.")
+
+age_in_5_years = int(age) + 5
+print(f"Em 5 anos, você terá {age_in_5_years} anos.")
